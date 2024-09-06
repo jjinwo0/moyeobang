0fb5886319b4a90ffcec114a08a28e1b9b692fec
jinwoo <zvzv9808@gmail.com>
2024-09-06 18:00:52 +0900

    [REFACTOR] #12 연관관계 엔티티명 변경

--- backend/src/main/java/com/ssafy/moyeobang/settle/adapter/out/persistence/travel/MemberTravelEntity.java
+++ backend/src/main/java/com/ssafy/moyeobang/settle/adapter/out/persistence/travel/MemberTravelEntity.java
@@ -4,20 +4,19 @@ import com.ssafy.moyeobang.settle.adapter.out.persistence.member.MemberEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.math.BigInteger;
-
 @Entity
 @Getter
 @Builder
+@Table(name = "member_travel")
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MemberTravel {
+public class MemberTravelEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_travel_id")
     private Long id;
 
-    private BigInteger balance;
+    private Integer balance;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
@@ -26,4 +25,12 @@ public class MemberTravel {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "travel_id")
     private TravelEntity travelEntity;
+
+    public void addBalance(Integer balance) {
+        this.balance += balance;
+    }
+
+    public void subtractBalance(Integer balance) {
+        this.balance -= balance;
+    }
 }
