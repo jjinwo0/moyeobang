0c066ff113f30ef865fe9165ea24534440a8bf84
유지연 <apple20011215@gmail.com>
2024-09-06 17:39:07 +0900

    [FEAT] #2 emotion zustand 초기설정

--- frontend/moyeobang/vite.config.ts
+++ frontend/moyeobang/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(
+    // emotion 설정
+    {
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin'],
+      },
+    }
+  )],
 })
