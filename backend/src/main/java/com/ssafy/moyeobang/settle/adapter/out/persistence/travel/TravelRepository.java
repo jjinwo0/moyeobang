981364e65d10492a05494d6f2e82331353e3ee7c
jinwoo <zvzv9808@gmail.com>
2024-09-06 18:03:29 +0900

    [REFACTOR] #12 여행 관련 outport, adapter 정의

---
+++ backend/src/main/java/com/ssafy/moyeobang/settle/adapter/out/persistence/travel/TravelRepository.java
@@ -0,0 +1,6 @@
+package com.ssafy.moyeobang.settle.adapter.out.persistence.travel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TravelRepository extends JpaRepository<TravelEntity, Long> {
+}
