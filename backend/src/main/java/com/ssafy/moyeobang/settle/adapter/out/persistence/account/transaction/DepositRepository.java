4c46333a864b8a9391716826a054383b16666ef3
jinwoo <zvzv9808@gmail.com>
2024-09-06 18:02:24 +0900

    [REFACTOR] #12 Transaction 분기로 인한 변경

--- backend/src/main/java/com/ssafy/moyeobang/settle/adapter/out/persistence/account/transaction/DepositRepository.java
+++ backend/src/main/java/com/ssafy/moyeobang/settle/adapter/out/persistence/account/transaction/DepositRepository.java
@@ -2,5 +2,5 @@ package com.ssafy.moyeobang.settle.adapter.out.persistence.account.transaction;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TransactionRepository extends JpaRepository<TransactionEntity, Long> {
+public interface DepositRepository extends JpaRepository<DepositEntity, Long> {
 }
