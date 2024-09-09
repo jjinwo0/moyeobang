a6bd11a5a0d3992e3f5221899d58b13b01f40816
김두열 <enduf768640@gmail.com>
2024-09-09 14:20:27 +0900

    [FIX] #20 공통 JPA 엔티티 오류 수정

--- backend/src/main/java/com/ssafy/moyeobang/common/persistenceentity/member/MemberAccountJpaEntity.java
+++ backend/src/main/java/com/ssafy/moyeobang/common/persistenceentity/member/MemberAccountJpaEntity.java
@@ -3,9 +3,12 @@ package com.ssafy.moyeobang.common.persistenceentity.member;
 import com.ssafy.moyeobang.common.persistenceentity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -27,9 +30,14 @@ public class MemberAccountJpaEntity extends BaseEntity {
 
     private String accountNumber;
 
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private MemberJpaEntity member;
+
     @Builder
-    public MemberAccountJpaEntity(String bankName, String accountNumber) {
+    public MemberAccountJpaEntity(String bankName, String accountNumber, MemberJpaEntity member) {
         this.bankName = bankName;
         this.accountNumber = accountNumber;
+        this.member = member;
     }
 }
