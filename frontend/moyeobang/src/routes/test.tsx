363bc9c0f331c38913d0b2ccd0e6589f43204f3b
전가현 <rkgus678@gmail.com>
2024-09-10 14:16:50 +0900

    [FEAT] #27 정산 말풍선 컴포넌트 생성

--- frontend/moyeobang/src/routes/test.tsx
+++ frontend/moyeobang/src/routes/test.tsx
@@ -1,38 +1,40 @@
-import React from 'react';
-
+/** @jsxImportSource @emotion/react */
 import {createFileRoute} from '@tanstack/react-router';
-import Btn from '@/components/common/btn/Btn';
-import TwoBtn from '@/components/common/btn/TwoBtn';
+import {useState} from 'react';
+import Navbar from '@/components/common/navBar/Navbar';
+import React from 'react';
+import {css} from '@emotion/react';
+import MessagePopup from '@/components/common/messagePopup/MessagePopup';
+import CalculatePopup from '@/components/common/calculate/CalculatePopup';
 import '@/styles/fonts.css';
 
+const message = css`
+  font-family: 'medium';
+`;
 
 export const Route = createFileRoute('/test')({
-  component: Test,
+  component: () => {
+    // useState를 함수 내부에서 선언합니다.
+    const [showCalculatePopup, setShowCalculatePopup] = useState(false);
+
+    return (
+      <>
+        {/* 정산 버튼 클릭 시 팝업 상태 변경 */}
+        <Navbar onCalClick={() => setShowCalculatePopup(!showCalculatePopup)} />
+
+        <MessagePopup
+          message={
+            <div css={message}>
+              <span>여기서 2명의 사람들이 평균</span>
+              <br />
+              <span style={{color: 'blue'}}>120000원</span> 사용했나방
+            </div>
+          }
+        />
+
+        {/* 상태가 true일 때만 CalculatePopup 표시 */}
+        {showCalculatePopup && <CalculatePopup />}
+      </>
+    );
+  },
 });
-
-function Test() {
-  return (
-    <>
-      <Btn buttonStyle={{size: 'small', style: 'red'}}>
-        확인
-      </Btn>
-      <Btn buttonStyle={{size: 'middle', style: 'blue'}}>
-        중간 버튼
-      </Btn>
-      <Btn buttonStyle={{size: 'big', style: 'blue'}}>
-        계좌인증 하기
-      </Btn>
-      <Btn buttonStyle={{size: 'big', style: 'blueOutlined'}}>
-        작은 버튼
-      </Btn>
-      <Btn buttonStyle={{size: 'middle', style: 'gray'}}>
-        작은 버튼
-      </Btn>
-      <TwoBtn leftText='예정 진행' rightText='지난 여행'></TwoBtn>
-      
-    </>
-  );
-}
-
-
-
