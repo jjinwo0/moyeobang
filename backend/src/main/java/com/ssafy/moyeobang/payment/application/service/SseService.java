ba2017511d89d1a2ac525ebe12e7a047e79e2dfa
gnsals0904 <gnsals0904@naver.com>
2024-09-10 17:50:21 +0900

    [REFACTOR] #5 SSE 연결 로직 수정

--- backend/src/main/java/com/ssafy/moyeobang/payment/application/service/SseService.java
+++ backend/src/main/java/com/ssafy/moyeobang/payment/application/service/SseService.java
@@ -3,7 +3,6 @@ package com.ssafy.moyeobang.payment.application.service;
 import com.ssafy.moyeobang.common.annotation.UseCase;
 import com.ssafy.moyeobang.payment.adapter.out.sse.SseEmitters;
 import com.ssafy.moyeobang.payment.application.port.in.SseUseCase;
-import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -17,15 +16,7 @@ public class SseService implements SseUseCase {
     public SseEmitter connect(String transactionId) {
         SseEmitter emitter = new SseEmitter(180000L); // 180초 3분 타임아웃
         sseEmitters.add(transactionId, emitter);
-        sseEmitters.sendEvent(emitter, "connect", "connected! Waiting for payment...");
-
-        try {
-            emitter.send(SseEmitter.event()
-                    .name("connect")
-                    .data("connected! Waiting for payment..."));
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to send initial SSE event", e);
-        }
+        sseEmitters.sendEvent(transactionId, "connect", "connected! Waiting for payment...");
         return emitter;
     }
 }
