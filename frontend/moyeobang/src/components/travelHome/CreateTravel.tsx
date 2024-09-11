6fae51f4811b17f7b81b6a6832b59971b7354856
gahyoenj <rkgus678@gmail.com>
2024-09-11 22:45:25 +0900

    [FEAT] #34 여행 만들기 사진 선택 기능 추가

--- frontend/moyeobang/src/components/travelHome/CreateTravel.tsx
+++ frontend/moyeobang/src/components/travelHome/CreateTravel.tsx
@@ -1,232 +1,3 @@
-// import {css} from '@emotion/react';
-// import React, {useState} from 'react';
-// import HeaderWithXButton from '../common/Header/HeaderWithXbutton';
-// import {colors} from '@/styles/colors';
-// import LabeledInput from '../common/Inputs/LabeledInput';
-// import LocationInput from '../common/Inputs/LocationInput';
-// import QuizInput from '../common/Inputs/QuizInput';
-// import AuthVerification from './AuthVerification';
-// import Btn from '../common/btn/Btn';
-// import addTravelPhoto from '@/assets/icons/addTravelPhoto.png';
-// import calendar from '@/assets/icons/calendar.png';
-
-// import DatePicker from 'react-datepicker';
-// import 'react-datepicker/dist/react-datepicker.css';
-
-// interface CreateTravelProps {
-//   onClose: () => void; // 모달을 닫는 함수
-// }
-
-// const modalStyle = css`
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   width: 100%;
-//   height: 100%;
-//   background-color: white;
-//   z-index: 10000;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: flex-start;
-// `;
-
-// const contentStyle = css`
-//   padding: 20px;
-//   flex-grow: 1; /* 남는 공간을 차지하게 만듦 */
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// const titleStyle = css`
-//   display: flex;
-//   justify-content: center;
-//   margin: 40px 0;
-//   font-family: 'bold';
-//   font-size: 24px;
-// `;
-
-// const titleBlue = css`
-//   color: ${colors.fifth};
-// `;
-
-// const travelStyle = css`
-//   font-family: 'regular';
-//   font-size: 20px;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   gap: 20px;
-//   flex-grow: 1; /* 여행 관련 입력란들이 남는 공간을 차지하고 버튼을 아래로 밀어냄 */
-// `;
-
-// const inputsContainerStyle = css`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 30px; /* 입력란들 사이의 간격을 30px로 설정 */
-// `;
-
-// const inputWithIconStyle = css`
-//   display: flex;
-//   align-items: center;
-//   position: relative;
-// `;
-
-// const calendarIconStyle = css`
-//   position: absolute;
-//   right: 20px;
-//   top: 65%;
-//   transform: translateY(-50%);
-//   width: 24px;
-//   height: 24px;
-// `;
-
-// const inputStyleWithIcon = css`
-//   padding-right: 40px; /* 아이콘 공간을 확보하기 위해 패딩 추가 */
-//   width: 100%;
-// `;
-
-// const quizStyle = css`
-//   display: flex;
-//   flex-direction: column;
-//   gap: 20px; /* 퀴즈 입력란들 사이의 간격을 20px로 설정 */
-//   margin-top: 10px;
-// `;
-
-// const btnContainerStyle = css`
-//   display: flex;
-//   justify-content: flex-end;
-//   margin-top: auto; /* 버튼을 아래로 배치 */
-//   margin-bottom: 45px;
-//   margin-right: 10px;
-// `;
-
-// const photoStyle = css`
-//   font-family: 'regular';
-//   align-self: flex-start; /* 사진 글씨를 왼쪽으로 정렬 */
-//   margin-left: 15px;
-//   margin-top: 10px;
-
-//   display: flex; /* 텍스트와 이미지를 수평으로 배치 */
-//   align-items: center; /* 텍스트와 이미지를 수평으로 맞춤 */
-
-//   img {
-//     width: 30px;
-//     height: 30px;
-//     margin-left: 10px;
-//     // margin-top: 1px; /* 이미지에만 margin-top 적용 */
-//     margin-bottom: 3px;
-//   }
-// `;
-
-// function CreateTravel({onClose}: CreateTravelProps) {
-//   const [step, setStep] = useState<number>(1);
-//   const [dateRange, setDateRange] = useState<[Date | null, Date | null]>([
-//     null,
-//     null,
-//   ]); // 날짜 범위
-//   const [startDate, endDate] = dateRange; // 시작일과 종료일
-
-//   const handleNextClick = () => {
-//     setStep(2); // "다음" 버튼 클릭 시 2단계(본인 인증)로 전환
-//   };
-
-//   const handleXClick = () => {
-//     onClose();
-//   };
-
-//   const handleCalendarClick = () => {
-//     // 캘린더 아이콘 클릭 시 DatePicker가 열리도록 포커스
-//     const input = document.getElementById('datepicker') as HTMLElement;
-//     input?.focus(); // input에 포커스를 주어 DatePicker 열기
-//   };
-
-//   return (
-//     <div css={modalStyle}>
-//       <HeaderWithXButton onXClick={handleXClick} />
-//       <div css={contentStyle}>
-//         <div css={titleStyle}>
-//           <span>여행</span>
-//           <span css={titleBlue}>만들어방</span>
-//         </div>
-
-//         {step === 1 ? (
-//           <>
-//             <div css={travelStyle}>
-//               <div css={inputsContainerStyle}>
-//                 <LabeledInput
-//                   label="여행이름"
-//                   placeholder="여행 이름을 입력하세요"
-//                 />
-//                 <div css={inputWithIconStyle}>
-//                   <DatePicker
-//                     selected={startDate}
-//                     onChange={(update: [Date | null, Date | null]) =>
-//                       setDateRange(update)
-//                     }
-//                     startDate={startDate}
-//                     endDate={endDate}
-//                     selectsRange
-//                     placeholderText="여행 기간을 선택하세요"
-//                     customInput={
-//                       <LabeledInput
-//                         label="여행기간"
-//                         placeholder="여행 기간을 선택하세요"
-//                       />
-//                     } // LabeledInput을 커스텀 input으로 사용
-//                   />
-//                   <img
-//                     src={calendar}
-//                     css={calendarIconStyle}
-//                     onClick={() => {
-//                       const input = document.querySelector(
-//                         '.react-datepicker__input-container input'
-//                       ) as HTMLElement;
-//                       input?.focus();
-//                     }} // 캘린더 아이콘 클릭 시 DatePicker가 열리도록 함
-//                   />
-//                 </div>
-//                 <LocationInput
-//                   label="여행장소"
-//                   placeholder="여행 장소를 검색하세요"
-//                 />
-//               </div>
-
-//               <div css={quizStyle}>
-//                 <QuizInput
-//                   title="초대퀴즈"
-//                   label="Q"
-//                   placeholder="김훈민의 별명은?"
-//                 />
-//                 <QuizInput label="A" placeholder="김훈남민" />
-//               </div>
-
-//               <div css={photoStyle}>
-//                 <span>사진</span>
-//                 <img src={addTravelPhoto} />
-//               </div>
-//             </div>
-
-//             <div css={btnContainerStyle}>
-//               <Btn
-//                 buttonStyle={{style: 'blue', size: 'small'}}
-//                 onClick={handleNextClick}
-//               >
-//                 다음
-//               </Btn>
-//             </div>
-//           </>
-//         ) : (
-//           <AuthVerification />
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default CreateTravel;
-
-// src/components/CreateTravel.tsx
-
 import {css} from '@emotion/react';
 import React, {useState, useRef, useEffect} from 'react';
 import HeaderWithXButton from '../common/Header/HeaderWithXbutton';
@@ -339,8 +110,8 @@ const photoStyle = css`
     width: 30px;
     height: 30px;
     margin-left: 10px;
-    /* margin-top: 1px; /* 이미지에만 margin-top 적용 */
-    margin-bottom: 3px;
+    cursor: pointer; /* 클릭 가능하게 설정 */
+    object-fit: cover; /* 이미지 크기에 맞게 조정 */
   }
 `;
 
@@ -351,13 +122,11 @@ function CreateTravel({onClose}: CreateTravelProps) {
     null,
   ]); // 날짜 범위
   const [isCalendarOpen, setIsCalendarOpen] = useState<boolean>(false); // 달력 표시 상태
+  const [selectedImage, setSelectedImage] = useState<string | null>(null); // 이미지 미리보기 상태
+  const fileInputRef = useRef<HTMLInputElement | null>(null); // 파일 입력 Ref
 
   const handleNextClick = () => {
     setStep(2); // "다음" 버튼 클릭 시 2단계(본인 인증)로 전환
-  };
-
-  const handleXClick = () => {
-    onClose();
   };
 
   const handleCalendarClick = () => {
@@ -367,6 +136,18 @@ function CreateTravel({onClose}: CreateTravelProps) {
   const handleSelectRange = (start: Date, end: Date) => {
     setDateRange([start, end]);
     setIsCalendarOpen(false); // 달력 닫기
+  };
+
+  const handlePhotoClick = () => {
+    fileInputRef.current?.click(); // 이미지 클릭 시 파일 선택창 열기
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const imageUrl = URL.createObjectURL(file);
+      setSelectedImage(imageUrl); // 선택한 이미지 미리보기
+    }
   };
 
   const handleClickOutside = (event: MouseEvent) => {
@@ -393,7 +174,7 @@ function CreateTravel({onClose}: CreateTravelProps) {
 
   return (
     <div css={modalStyle}>
-      <HeaderWithXButton onXClick={handleXClick} />
+      <HeaderWithXButton onXClick={onClose} />
       <div css={contentStyle}>
         <div css={titleStyle}>
           <span>여행</span>
@@ -453,7 +234,17 @@ function CreateTravel({onClose}: CreateTravelProps) {
 
               <div css={photoStyle}>
                 <span>사진</span>
-                <img src={addTravelPhoto} alt="Add Photo" />
+                <img
+                  src={selectedImage || addTravelPhoto}
+                  alt="사진 추가"
+                  onClick={handlePhotoClick}
+                />
+                <input
+                  type="file"
+                  ref={fileInputRef}
+                  style={{display: 'none'}}
+                  onChange={handleFileChange}
+                />
               </div>
             </div>
 
