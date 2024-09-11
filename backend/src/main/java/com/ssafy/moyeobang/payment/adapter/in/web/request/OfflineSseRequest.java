05ac9ad678adb678d83d4c4ddf05c31bc9d60af6
gnsals0904 <gnsals0904@naver.com>
2024-09-11 17:50:19 +0900

    [REFACTOR] #5 SSE 연결 요청 스펙 변경

--- backend/src/main/java/com/ssafy/moyeobang/payment/adapter/in/web/request/OfflineSseRequest.java
+++ backend/src/main/java/com/ssafy/moyeobang/payment/adapter/in/web/request/OfflineSseRequest.java
@@ -1,4 +1,4 @@
 package com.ssafy.moyeobang.payment.adapter.in.web.request;
 
-public record OfflineSseRequest(String paymentSessionId) {
+public record OfflineSseRequest(String paymentSessionId, String travelAccountNumber) {
 }
