e5f51eb72ad02ae2aaad04878d699fda0abffc03
gnsals0904 <gnsals0904@naver.com>
2024-09-11 17:50:33 +0900

    [REFACTOR] #5 현장 결제 요청, 로직 수정

--- backend/src/main/java/com/ssafy/moyeobang/payment/adapter/in/server/request/OfflinePaymentRequest.java
+++ backend/src/main/java/com/ssafy/moyeobang/payment/adapter/in/server/request/OfflinePaymentRequest.java
@@ -1,4 +1,14 @@
 package com.ssafy.moyeobang.payment.adapter.in.server.request;
 
-public record OfflinePaymentRequest(String accountNumber, String paymentSessionId, Integer amount, String paymentLocation, ) {
+import java.util.List;
+
+public record OfflinePaymentRequest(
+        String title,
+        Integer amount,
+        String senderAccountNumber,
+        String receiverAccountNumber,
+        Float latitude,
+        Float longitude,
+        String paymentSessionId,
+        List<OrderRequest> orders) {
 }
--- backend/src/main/java/com/ssafy/moyeobang/payment/application/service/OfflinePaymentService.java
+++ backend/src/main/java/com/ssafy/moyeobang/payment/application/service/OfflinePaymentService.java
@@ -2,13 +2,14 @@ package com.ssafy.moyeobang.payment.application.service;
 
 import com.ssafy.moyeobang.common.annotation.UseCase;
 import com.ssafy.moyeobang.payment.adapter.in.server.request.OfflinePaymentRequest;
-import com.ssafy.moyeobang.payment.adapter.out.persistence.MemberAccountRepositoryInPayment;
-import com.ssafy.moyeobang.payment.application.domain.Account;
+import com.ssafy.moyeobang.payment.adapter.out.persistence.account.MemberAccountRepositoryInPayment;
 import com.ssafy.moyeobang.payment.application.domain.Member;
-import com.ssafy.moyeobang.payment.application.domain.Money;
 import com.ssafy.moyeobang.payment.application.domain.Payment;
+import com.ssafy.moyeobang.payment.application.domain.PaymentReceipt;
 import com.ssafy.moyeobang.payment.application.error.InsufficientBalanceException;
 import com.ssafy.moyeobang.payment.application.port.in.OfflinePaymentUseCase;
+import com.ssafy.moyeobang.payment.application.port.out.SaveOrdersPort;
+import com.ssafy.moyeobang.payment.application.port.out.SaveWithdrawPort;
 import com.ssafy.moyeobang.payment.application.port.out.SsePort;
 import lombok.RequiredArgsConstructor;
 
@@ -17,13 +18,15 @@ import lombok.RequiredArgsConstructor;
 public class OfflinePaymentService implements OfflinePaymentUseCase {
 
     private final SsePort ssePort;
+    private final SaveWithdrawPort saveWithdrawPort;
+    private final SaveOrdersPort saveOrdersPort;
     private final MemberAccountRepositoryInPayment memberAccountRepository;
     private final String paymentSystem = "ssafy_bank";
 
     @Override
     public boolean confirmPayment(OfflinePaymentRequest request) throws InsufficientBalanceException {
         Member member = new Member("test");
-        boolean paymentSuccess = processPayment(member.getMemberKey(), request.accountNumber(), request.amount());
+        boolean paymentSuccess = processPayment(request);
         if (paymentSuccess) {
             ssePort.sendPaymentSuccess(request.paymentSessionId(), "Payment successfully");
         } else {
@@ -33,12 +36,19 @@ public class OfflinePaymentService implements OfflinePaymentUseCase {
     }
 
 
-    private boolean processPayment(long senderAccountId, String receiverAccountId, long amount)
+    private boolean processPayment(OfflinePaymentRequest request)
             throws InsufficientBalanceException {
-        Account senderAccount = memberAccountRepository.findById(senderAccountId);
-        Money money = Money.of(amount);
+        Payment payment = Payment.of(request.senderAccountNumber(), request.receiverAccountNumber(), request.amount());
 
-        Payment payment = Payment.of(senderAccount, receiverAccountId, money);
-        payment.processPayment();
+        PaymentReceipt paymentReceipt = payment.processPayment(request.paymentSessionId(), request.orders(),
+                request.longitude(), request.latitude(),
+                request.title());
+
+        saveWithdrawPort.saveWithdraw(paymentReceipt.getWithdraw());
+
+        saveOrdersPort.saveOrders(paymentReceipt.getOrders());
+
+        // TODO : 변경 예정
+        return true;
     }
 }
