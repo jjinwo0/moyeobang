fcb9771512e106b79464bb17e1cc9b3ce84b5efb
gahyoenj <rkgus678@gmail.com>
2024-09-13 02:04:01 +0900

    [FEAT] #42 알림 모달 UI

--- frontend/moyeobang/src/components/notification/NotificationModal.tsx
+++ frontend/moyeobang/src/components/notification/NotificationModal.tsx
@@ -1,13 +1,15 @@
 import {css} from '@emotion/react';
 import React from 'react';
 import HeaderWithXButton from '../common/Header/HeaderWithXbutton';
+import HurryNotification from './HurryNotification';
+import PublicRequest from './PublicRequest';
 
 interface NotificationModalProps {
   onXClick: () => void;
 }
 
 const layoutStyle = css`
-  // margin-top: 50px;
+  margin-top: 70px;
   position: fixed;
   top: 0;
   left: 0;
@@ -19,12 +21,22 @@ const layoutStyle = css`
   z-index: 100;
 `;
 
+const notificationStyle = css`
+  display: flex;
+  flex-direction: column; /* 세로로 나열 */
+  gap: 10px; /* 컴포넌트들 사이 간격을 20px로 설정 */
+  width: 100%; /* 필요한 경우 너비 설정 */
+`;
+
 export default function NotificationModal({onXClick}: NotificationModalProps) {
   return (
     <>
+      <HeaderWithXButton onXClick={onXClick} />
       <div css={layoutStyle}>
-        <HeaderWithXButton onXClick={onXClick} />
-        <div>알림모달</div>
+        <div css={notificationStyle}>
+          <HurryNotification />
+          <PublicRequest />
+        </div>
       </div>
     </>
   );
--- frontend/moyeobang/src/components/notification/PublicRequest.tsx
+++ frontend/moyeobang/src/components/notification/PublicRequest.tsx
@@ -1,6 +1,70 @@
 import {css} from '@emotion/react';
 import React from 'react';
+import siren from '@/assets/icons/siren.webp';
+import Btn from '../common/btn/Btn';
+
+const containerStyle = css`
+  width: 100%;
+  height: 90px;
+`;
+
+const contentStyle = css`
+  display: flex; /* 이미지와 텍스트를 가로로 배치 */
+  align-items: center; /* 세로 중앙 정렬 */
+  padding: 20px;
+
+  img {
+    width: 34px;
+    margin-right: 20px; /* 이미지와 텍스트 사이 간격 */
+  }
+`;
+
+const titleStyle = css`
+  font-family: 'semibold';
+  font-size: 18px;
+`;
+
+const textStyle = css`
+  font-family: 'regular';
+  font-size: 18px;
+`;
+
+const buttonContainerStyle = css`
+  display: flex; /* 버튼들을 가로로 배치 */
+  gap: 10px; /* 버튼 사이 간격 추가 */
+  margin-top: 10px; /* 텍스트와 버튼 사이 간격 추가 */
+`;
+
+const timeStyle = css`
+  font-family: 'regular';
+  font-size: 12px;
+  margin-bottom: 5px;
+`;
+
+const travelName: string = '아기돼지오형제';
+
+const nickName: Nickname = '훈민';
 
 export default function PublicRequest() {
-  return <></>;
+  return (
+    <div css={containerStyle}>
+      <div css={contentStyle}>
+        <img src={siren} alt="Notification Icon" />
+        <div>
+          <p css={timeStyle}>2024.09.03 13:10</p>
+          <span css={titleStyle}>{travelName}</span>
+          <span css={textStyle}>
+            에서 <span css={titleStyle}>{nickName}</span>님의 몫이
+            300,000원(20%) 밖에 남지 않았어요!
+            <br />
+            개인입금을 통해 통장에 돈을 채워주세요!
+          </span>
+          <div css={buttonContainerStyle}>
+            <Btn buttonStyle={{style: 'red', size: 'tiny'}}>취소</Btn>
+            <Btn buttonStyle={{style: 'blue', size: 'tiny'}}>수락</Btn>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
