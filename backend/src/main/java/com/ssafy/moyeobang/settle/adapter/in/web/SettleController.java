bf389ea8a3a94dd952a513f87a23c5ecd111ba7b
jinwoo <zvzv9808@gmail.com>
2024-09-12 10:08:50 +0900

    [REFACTOR] #38 정산 API 스펙 수정

--- backend/src/main/java/com/ssafy/moyeobang/settle/adapter/in/web/SettleController.java
+++ backend/src/main/java/com/ssafy/moyeobang/settle/adapter/in/web/SettleController.java
@@ -1,36 +1,35 @@
 package com.ssafy.moyeobang.settle.adapter.in.web;
 
+import static com.ssafy.moyeobang.common.util.ApiUtils.success;
+
 import com.ssafy.moyeobang.common.annotation.WebAdapter;
-import com.ssafy.moyeobang.common.util.ApiUtils;
 import com.ssafy.moyeobang.common.util.ApiUtils.ApiResult;
 import com.ssafy.moyeobang.settle.adapter.in.web.request.SettleRequest;
 import com.ssafy.moyeobang.settle.application.port.in.SettleCommand;
 import com.ssafy.moyeobang.settle.application.port.in.SettleUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import static com.ssafy.moyeobang.common.util.ApiUtils.error;
-import static com.ssafy.moyeobang.common.util.ApiUtils.success;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @WebAdapter
 @RestController
-@RequestMapping("/api/travel/accounts")
+@RequestMapping("/api/travel/accounts/transactions")
 @RequiredArgsConstructor
 public class SettleController {
 
     private final SettleUseCase settleUseCase;
 
-    @PostMapping("/{accountId}/transactions/{transactionId}/settle")
-    public ApiResult<Boolean> settle(@PathVariable("accountId") Long accountId,
-                               @PathVariable("transactionId") Long transactionId,
-                               @RequestBody SettleRequest request) {
+    @PostMapping("/{transactionId}/settle")
+    public ApiResult<Boolean> settle(@PathVariable("transactionId") Long transactionId,
+                                     @RequestBody SettleRequest request) {
 
         // todo: 리턴값 고정하고 값이 없을 때 errorResponse를 할 것인가?
         return success(request.orderItems().stream()
                 .allMatch(item -> settleUseCase.balanceSettle(
                         new SettleCommand(
-                                accountId,
                                 transactionId,
                                 item.title(),
                                 item.amount(),
