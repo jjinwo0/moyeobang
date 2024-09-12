2358b698bc9ae9400ae3495993583c37688fda80
gahyoenj <rkgus678@gmail.com>
2024-09-12 23:58:57 +0900

    [FEAT] #39 hideHeader profile 경로 추가

--- frontend/moyeobang/src/routes/_layout/_protected/_layout.tsx
+++ frontend/moyeobang/src/routes/_layout/_protected/_layout.tsx
@@ -1,47 +1,55 @@
-import { createFileRoute, Outlet, useLocation } from '@tanstack/react-router';
+import {createFileRoute, Outlet, useLocation} from '@tanstack/react-router';
 import HeaderWithAlarmAndQR from '@/components/common/Header/HeaderWithAlarmAndQR';
 import React from 'react';
-import { css } from '@emotion/react';
-import { useState } from "react";
-import PayModal from "@/components/Account/PayModal/PayModal";
-
+import {css} from '@emotion/react';
+import {useState} from 'react';
+import PayModal from '@/components/Account/PayModal/PayModal';
 
 export const Route = createFileRoute('/_layout/_protected/_layout')({
-  component: Header
-})
+  component: Header,
+});
 
 const layoutStyle = css`
-  display:flex;
-  flex-direction:column;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default function Header() {
-  const [ isQROpen, setIsQROpen ] = useState(false);
-  const [ isAlarmOpen, setIsAlarmOpen ] = useState(false);
+  const [isQROpen, setIsQROpen] = useState(false);
+  const [isAlarmOpen, setIsAlarmOpen] = useState(false);
 
   const {pathname} = useLocation();
 
-  const hideHeader = pathname === '/account/receipt' || pathname === '/account/calculate' || pathname.startsWith('/account/detail/');
+  const hideHeader =
+    pathname === '/account/receipt' ||
+    pathname === '/account/calculate' ||
+    pathname.startsWith('/account/detail/') ||
+    pathname === '/profile';
   function handleAlarmClick() {
     setIsAlarmOpen(true);
-  
   }
 
   function handleQRClick() {
-    setIsQROpen((prev) => !prev);
+    setIsQROpen(prev => !prev);
   }
 
   return (
-     <>
-        { !hideHeader &&  <HeaderWithAlarmAndQR onAlarmClick={handleAlarmClick} onQRClick={handleQRClick} />}
-        <div css={layoutStyle}>
-        {isQROpen ? <PayModal
-          onXClick={handleQRClick}/> : 
+    <>
+      {!hideHeader && (
+        <HeaderWithAlarmAndQR
+          onAlarmClick={handleAlarmClick}
+          onQRClick={handleQRClick}
+        />
+      )}
+      <div css={layoutStyle}>
+        {isQROpen ? (
+          <PayModal onXClick={handleQRClick} />
+        ) : (
           <>
-          <Outlet/>
+            <Outlet />
           </>
-          }
-          </div>
-      </>
-  )
+        )}
+      </div>
+    </>
+  );
 }
