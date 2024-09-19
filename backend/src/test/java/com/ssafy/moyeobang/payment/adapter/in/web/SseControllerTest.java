7928fbb22eb29f233eb6479923801fba6faed6ca
gnsals0904 <gnsals0904@naver.com>
2024-09-20 00:36:17 +0900

    [REFACTOR] #5 SSE 컨트롤러 connect Test 완료

--- backend/src/test/java/com/ssafy/moyeobang/payment/adapter/in/web/SseControllerTest.java
+++ backend/src/test/java/com/ssafy/moyeobang/payment/adapter/in/web/SseControllerTest.java
@@ -2,6 +2,7 @@ package com.ssafy.moyeobang.payment.adapter.in.web;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
@@ -12,6 +13,7 @@ import com.ssafy.moyeobang.support.WebAdapterTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 public class SseControllerTest extends WebAdapterTestSupport {
@@ -19,7 +21,7 @@ public class SseControllerTest extends WebAdapterTestSupport {
     @MockBean
     private SseUseCase sseUseCase;
 
-    @DisplayName("결제 요청이 들어오면 SSE 연결이 성공적으로 이루어진다.")
+    @DisplayName("결제 요청이 들어오기전, SSE 연결이 이루어져야 한다")
     @Test
     void testSseConnect() throws Exception {
         // Given
@@ -29,11 +31,21 @@ public class SseControllerTest extends WebAdapterTestSupport {
         when(sseUseCase.connect(paymentRequestId)).thenReturn(emitter);
 
         // Mock SSE 연결
-        mockMvc.perform(get("/api/payment/connect")
+        MvcResult mvcResult = mockMvc.perform(get("/api/payment/connect")
                         .param("paymentRequestId", paymentRequestId)
                         .accept(TEXT_EVENT_STREAM_VALUE))
                 .andExpect(request().asyncStarted())
+                .andReturn();
+
+        emitter.send(SseEmitter.event().name("connect").data("connected!"));
+
+        emitter.complete();
+
+        mockMvc.perform(asyncDispatch(mvcResult))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(TEXT_EVENT_STREAM_VALUE));
+                .andExpect(content().contentTypeCompatibleWith(TEXT_EVENT_STREAM_VALUE))
+                .andExpect(content().string("connected!"));
+
     }
+
 }
