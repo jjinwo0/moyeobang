4d90ea3097850e33692fa54aa1a6b720bac4cbfc
gnsals0904 <gnsals0904@naver.com>
2024-09-19 10:09:41 +0900

    [FEAT] #5 SSAFY BANK 관련 util 클래스 추가

---
+++ backend/src/main/java/com/ssafy/moyeobang/payment/adapter/out/bank/RestClientUtils.java
@@ -0,0 +1,57 @@
+package com.ssafy.moyeobang.payment.adapter.out.bank;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.List;
+import org.springframework.web.client.RestClient;
+
+abstract class RestClientUtils {
+
+    private static final ObjectMapper MAPPER = objectMapper();
+
+    public static JsonNode post(String uri, Object request) {
+        try {
+            String responseBody = restClient().post()
+                    .uri(uri)
+                    .contentType(APPLICATION_JSON)
+                    .body(request)
+                    .retrieve()
+                    .body(String.class);
+
+            return MAPPER.readTree(responseBody);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void postWithoutResponse(String uri, Object request) {
+        restClient().post()
+                .uri(uri)
+                .contentType(APPLICATION_JSON)
+                .body(request)
+                .retrieve();
+    }
+
+    public static <T> List<T> postConvertResponseToList(String uri, Object request, Class<T> clazz) {
+        return MAPPER.convertValue(
+                post(uri, request).path("REC").path("list"),
+                MAPPER.getTypeFactory().constructCollectionType(List.class, clazz)
+        );
+    }
+
+    private static RestClient restClient() {
+        return RestClient.builder()
+                .baseUrl("https://finopenapi.ssafy.io/ssafy/api/v1/edu")
+                .build();
+    }
+
+    private static ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        return objectMapper;
+    }
+}
