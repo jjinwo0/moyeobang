bcd7ff2f8e23888550899c28d4166f9e42d33a02
Jinwoo <zvzv9808@gmail.com>
2024-09-23 04:31:04 +0900

    [REFACTOR] #65 커스텀 정산 RequestBody 스펙 수정

--- backend/src/main/java/com/ssafy/moyeobang/settle/adapter/in/web/request/CustomSettleRequest.java
+++ backend/src/main/java/com/ssafy/moyeobang/settle/adapter/in/web/request/CustomSettleRequest.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.List;
 
-public record CustomSettleRequest(@NotNull String title,
-                                  @NotNull @Size(min = 1) List<CustomSettleInfo> customSettleInfos) {
+public record CustomSettleRequest(@NotNull String paymentName,
+                                  @NotNull int money,
+                                  @NotNull @Size(min = 1) List<CustomSettleInfo> info,
+                                  String splitMethod) {
 }
