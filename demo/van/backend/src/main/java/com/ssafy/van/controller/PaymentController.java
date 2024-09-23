2349663b74cd1daaf6399a870b265cebdd8f8448
gnsals0904 <gnsals0904@naver.com>
2024-09-23 17:21:06 +0900

    [FEAT] 모의 VAN 백엔드 서버 구현

---
+++ demo/van/backend/src/main/java/com/ssafy/van/controller/PaymentController.java
@@ -0,0 +1,36 @@
+package com.ssafy.van.controller;
+
+
+import com.ssafy.van.domain.ApiResult;
+import com.ssafy.van.domain.PaymentRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Controller
+@RequestMapping("/van/payment")
+@RequiredArgsConstructor
+public class PaymentController {
+    private final RestTemplate restTemplate;
+
+    @PostMapping("/process")
+    public ResponseEntity<ApiResult> processPayment(@RequestBody PaymentRequest paymentRequest) {
+        String mainServiceUrl = "http://localhost:8080/api/payment/confirm";
+        log.info("Payment request: {}", paymentRequest);
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(mainServiceUrl, paymentRequest, String.class);
+            ApiResult apiResult = new ApiResult("SUCCESS", response.getBody(), null);
+            return ResponseEntity.ok(apiResult);
+        } catch (Exception e) {
+            ApiResult errorResponse = new ApiResult("ERROR", null, "Payment failed.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+        }
+    }
+}
