e0c16b6768be32d838493ace951f8891df35279b
김두열 <enduf768640@gmail.com>
2024-09-24 17:23:51 +0900

    [FEAT] #82 여행 생성 이미지 저장 로컬 어댑터 구현
    - S3 이미지 업로드 어댑터로 변경 예정

---
+++ backend/src/main/java/com/ssafy/moyeobang/travel/adapter/out/local/LocalFileStorage.java
@@ -0,0 +1,52 @@
+package com.ssafy.moyeobang.travel.adapter.out.local;
+
+import static java.util.UUID.randomUUID;
+import static org.springframework.util.StringUtils.getFilenameExtension;
+
+import com.ssafy.moyeobang.common.annotation.PersistenceAdapter;
+import com.ssafy.moyeobang.travel.application.domain.BackgroundImage;
+import com.ssafy.moyeobang.travel.application.port.out.UploadImagePort;
+import com.ssafy.moyeobang.travel.error.FileStoreException;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import org.springframework.beans.factory.annotation.Value;
+
+@PersistenceAdapter
+public class LocalFileStorage implements UploadImagePort {
+
+    @Value("{storage.path}")
+    private String storagePath;
+
+    @Override
+    public String uploadImage(BackgroundImage image) {
+        String storeFileName = createStoreFileName(image.fileName());
+        String fullPath = createFullPath(storeFileName);
+
+        File file = new File(fullPath);
+
+        try (OutputStream outputStream = new FileOutputStream(file);
+             InputStream inputStream = image.inputStream()) {
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+
+            return fullPath;
+        } catch (IOException e) {
+            throw new FileStoreException();
+        }
+    }
+
+    private String createStoreFileName(String originalFileName) {
+        return randomUUID() + "." + getFilenameExtension(originalFileName);
+    }
+
+    private String createFullPath(String fileName) {
+        return storagePath + "/" + fileName;
+    }
+}
