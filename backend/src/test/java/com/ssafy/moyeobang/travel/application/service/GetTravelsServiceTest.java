a5534bd07bf2f2b445e7d7a5569768d007e0e617
enduf7686 <enduf768640@gmail.com>
2024-09-24 23:21:37 +0900

    [TEST] #84 여행 목록 조회 비즈니스 로직 테스트 작성
    - AutoParams 라이브러리를 사용하여 테스트 케이스 생성

---
+++ backend/src/test/java/com/ssafy/moyeobang/travel/application/service/GetTravelsServiceTest.java
@@ -0,0 +1,44 @@
+package com.ssafy.moyeobang.travel.application.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import autoparams.AutoSource;
+import autoparams.Repeat;
+import com.ssafy.moyeobang.travel.adapter.in.web.response.GetTravelsResponse;
+import com.ssafy.moyeobang.travel.application.domain.Travel;
+import com.ssafy.moyeobang.travel.application.port.out.LoadTravelPort;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+
+class GetTravelsServiceTest {
+
+    private final LoadTravelPort loadTravelPort = mock(LoadTravelPort.class);
+
+    private final GetTravelsService getTravelsService = new GetTravelsService(loadTravelPort);
+
+    @DisplayName("특정 회원이 소속되어 있는 여행 목록을 조회한다.")
+    @ParameterizedTest
+    @AutoSource
+    @Repeat
+    void getTravels(Long memberId, Travel travel1, Travel travel2, Travel travel3) {
+        //given
+        given(loadTravelPort.loadTravelsBy(memberId))
+                .willReturn(List.of(travel1, travel2, travel3));
+
+        //when
+        List<GetTravelsResponse> responses = getTravelsService.getTravels(memberId);
+
+        //then
+        assertThat(responses).extracting(
+                "travelId", "travelName", "travelImg", "participantCount"
+        ).containsExactlyInAnyOrder(
+                tuple(travel1.id(), travel1.title(), travel1.backgroundImageUrl(), travel1.getParticipantCount()),
+                tuple(travel2.id(), travel2.title(), travel2.backgroundImageUrl(), travel2.getParticipantCount()),
+                tuple(travel3.id(), travel3.title(), travel3.backgroundImageUrl(), travel3.getParticipantCount())
+        );
+    }
+}
