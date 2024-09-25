35efee385fac62da1c3d64e0647879dd2c4606a4
gnsals0904 <gnsals0904@naver.com>
2024-09-25 13:56:03 +0900

    [FEAT] 1/n 정산 로직 에러 코드 추가

--- backend/src/main/java/com/ssafy/moyeobang/payment/error/ErrorCode.java
+++ backend/src/main/java/com/ssafy/moyeobang/payment/error/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     TRAVEL_ACCOUNT_NOT_FOUND(HttpStatus.NOT_FOUND, 2001, "여행 계좌를 찾을 수 없습니다."),
-    TRAVEL_ACCOUNT_CANNOT_ACCESS(HttpStatus.BAD_REQUEST, 2002, "싸피 뱅크에서 해당 계좌에 접근할 수 없습니다.");
+    TRAVEL_ACCOUNT_CANNOT_ACCESS(HttpStatus.BAD_REQUEST, 2002, "싸피 뱅크에서 해당 계좌에 접근할 수 없습니다."),
+    NO_MEMBER_IN_TRAVEL(HttpStatus.NOT_FOUND, 2003, "여행 계좌에 참여된 참여자들을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final int code;
