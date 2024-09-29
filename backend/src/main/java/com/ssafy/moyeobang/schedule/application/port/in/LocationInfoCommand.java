feceb481484b152d0f8b6c112690b202782e70d7
gnsals0904 <gnsals0904@naver.com>
2024-09-30 02:59:56 +0900

    [FEAT] #115 새로운 일정 저장 도메인 구현

--- backend/src/main/java/com/ssafy/moyeobang/schedule/application/port/in/LocationInfoCommand.java
+++ backend/src/main/java/com/ssafy/moyeobang/schedule/application/port/in/LocationInfoCommand.java
@@ -2,9 +2,8 @@ package com.ssafy.moyeobang.schedule.application.port.in;
 
 import com.ssafy.moyeobang.schedule.adapter.in.web.request.LocationInfo;
 import jakarta.validation.constraints.NotNull;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+
 public record LocationInfoCommand(@NotNull String googlePlaceId, @NotNull String title, @NotNull String address,
                                   @NotNull double lat, @NotNull double lng,
                                   @NotNull String category) {
