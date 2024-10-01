959ac608d6616c2c67e865272936824e80c64391
gnsals0904 <gnsals0904@naver.com>
2024-10-02 00:20:44 +0900

    [REFACTOR] #118 native 쿼리 구현 구현

--- backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/out/persistence/schedule/ScheduleRepositoryInSchedule.java
+++ backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/out/persistence/schedule/ScheduleRepositoryInSchedule.java
@@ -1,10 +1,24 @@
 package com.ssafy.moyeobang.schedule.adapter.out.persistence.schedule;
 
 import com.ssafy.moyeobang.common.persistenceentity.schedule.ScheduleJpaEntity;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ScheduleRepositoryInSchedule extends JpaRepository<ScheduleJpaEntity, Long> {
     Optional<List<ScheduleJpaEntity>> findByTravelId(Long scheduleId);
+
+    @Query("SELECT s FROM ScheduleJpaEntity s " +
+            "LEFT JOIN FETCH s.withdraw w " +
+            "LEFT JOIN FETCH w.orderJpaEntities o " +
+            "LEFT JOIN FETCH o.memberOrderHistoryJpaEntities moh " +
+            "WHERE s.travel.id = :travelId " +
+            "ORDER BY s.sequence ASC")
+    List<ScheduleJpaEntity> findSchedulesByTravelIdAndDate(
+            @Param("travelId") Long travelId,
+            @Param("date") LocalDate date
+    );
 }
