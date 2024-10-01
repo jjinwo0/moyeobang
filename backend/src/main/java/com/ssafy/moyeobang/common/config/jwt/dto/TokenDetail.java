9da5afaa475b35c1ff14978d7ed7df02dbf60a8e
Jinwoo <zvzv9808@gmail.com>
2024-10-01 16:16:05 +0900

    [REFACTOR] #124 record type getter 삭제

--- backend/src/main/java/com/ssafy/moyeobang/common/config/jwt/dto/TokenDetail.java
+++ backend/src/main/java/com/ssafy/moyeobang/common/config/jwt/dto/TokenDetail.java
@@ -1,8 +1,6 @@
 package com.ssafy.moyeobang.common.config.jwt.dto;
 
 import java.time.LocalDateTime;
-import lombok.Getter;
 
-@Getter
 public record TokenDetail(String token, LocalDateTime expireTime) {
 }
