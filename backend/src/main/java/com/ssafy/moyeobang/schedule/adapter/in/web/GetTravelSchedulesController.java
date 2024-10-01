5c3febf237e62ffb4bd29f2487fda7cd5f2d1374
gnsals0904 <gnsals0904@naver.com>
2024-10-02 00:20:31 +0900

    [REFACTOR] #118 컨트롤러 구현

---
+++ backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/in/web/GetTravelSchedulesController.java
@@ -0,0 +1,28 @@
+package com.ssafy.moyeobang.schedule.adapter.in.web;
+
+import static com.ssafy.moyeobang.common.util.ApiUtils.success;
+
+import com.ssafy.moyeobang.common.annotation.WebAdapter;
+import com.ssafy.moyeobang.common.util.ApiUtils.ApiResult;
+import com.ssafy.moyeobang.schedule.adapter.in.web.response.DayScheduleResponse;
+import com.ssafy.moyeobang.schedule.application.port.in.GetTravelSchedulesUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@WebAdapter
+@RestController
+@RequestMapping("/api/travel")
+@RequiredArgsConstructor
+public class GetTravelSchedulesController {
+
+    private final GetTravelSchedulesUseCase getTravelSchedulesUseCase;
+
+    @GetMapping("/{travelId}/schedules")
+    public ApiResult<DayScheduleResponse> getTravelSchedules(@PathVariable long travelId) {
+        DayScheduleResponse dayScheduleResponse = getTravelSchedulesUseCase.getTravelSchedules(travelId);
+        return success(dayScheduleResponse);
+    }
+}
