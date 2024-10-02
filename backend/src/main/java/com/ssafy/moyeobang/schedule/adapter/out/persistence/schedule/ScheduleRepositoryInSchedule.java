ee12a2a0ccd06633a659b222f9b6b7d0566724d6
gnsals0904 <gnsals0904@naver.com>
2024-10-03 01:10:42 +0900

    [REFACTOR] #118 기본 Jpa 레포지토리 정의

--- backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/out/persistence/schedule/ScheduleRepositoryInSchedule.java
+++ backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/out/persistence/schedule/ScheduleRepositoryInSchedule.java
@@ -1,11 +1,7 @@
 package com.ssafy.moyeobang.schedule.adapter.out.persistence.schedule;
 
-import com.ssafy.moyeobang.common.persistenceentity.schedule.ScheduleJpaEntity;
 import java.util.List;
-import java.util.Optional;
 
 public interface ScheduleRepositoryInSchedule {
-    Optional<List<ScheduleJpaEntity>> findByTravelId(Long scheduleId);
-
-    List<Schedule> findSchedulesByTravelId(Long travelId);
+    List<ScheduleInfo> findSchedulesByTravelId(Long travelId);
 }
