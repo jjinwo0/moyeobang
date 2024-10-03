23bcd3432582cf72b47230255c91c36024f095aa
dhkang0912 <dhkang960912@gmail.com>
2024-10-04 01:08:40 +0900

    [FEAT] #133 config 버전 추가, 수정 필요

--- frontend/moyeobang/vite.config.ts
+++ frontend/moyeobang/vite.config.ts
@@ -1,34 +1,53 @@
+// import {defineConfig} from 'vite';
+// import react from '@vitejs/plugin-react';
+// import {TanStackRouterVite} from '@tanstack/router-vite-plugin';
+// import * as path from 'path';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     // emotion 설정
+//     react({
+//       jsxImportSource: '@emotion/react',
+//       babel: {
+//         plugins: ['@emotion/babel-plugin'],
+//       },
+//     }),
+//     // TanStack 설정
+//     TanStackRouterVite(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'), // @를 src로 설정
+//     },
+//   },
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://vu6tvl2vzm.apigw.ntruss.com',
+//         changeOrigin: true,
+//         rewrite: path => path.replace(/^\/api/, ''),
+//         secure: false, // HTTPS 검증 비활성화
+//       },
+//     },
+//   },
+// });
+
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import {TanStackRouterVite} from '@tanstack/router-vite-plugin';
-import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // emotion 설정
     react({
       jsxImportSource: '@emotion/react',
       babel: {
         plugins: ['@emotion/babel-plugin'],
       },
     }),
-    // TanStack 설정
-    TanStackRouterVite(),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // @를 src로 설정
-    },
-  },
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://vu6tvl2vzm.apigw.ntruss.com',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-        secure: false, // HTTPS 검증 비활성화
-      },
-    },
+    port: 5500,
   },
+  publicDir: './public',
 });
