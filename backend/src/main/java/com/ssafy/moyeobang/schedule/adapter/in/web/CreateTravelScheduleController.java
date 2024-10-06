4af0522f118d77e35c9c02fbf549119fd8b49f44
gnsals0904 <gnsals0904@naver.com>
2024-10-06 15:42:41 +0900

    [FEAT] #157 이미지 업로드 request 구현

--- backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/in/web/CreateTravelScheduleController.java
+++ backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/in/web/CreateTravelScheduleController.java
@@ -7,12 +7,14 @@ import com.ssafy.moyeobang.common.util.ApiUtils.ApiResult;
 import com.ssafy.moyeobang.schedule.adapter.in.web.request.CreateTravelScheduleRequest;
 import com.ssafy.moyeobang.schedule.application.port.in.CreateTravelScheduleCommand;
 import com.ssafy.moyeobang.schedule.application.port.in.CreateTravelScheduleUseCase;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @WebAdapter
@@ -24,10 +26,12 @@ public class CreateTravelScheduleController {
     private final CreateTravelScheduleUseCase createTravelScheduleUseCase;
 
     @PostMapping("/{travelId}/schedule")
-    public ApiResult<Boolean> createTravelSchedule(@PathVariable long travelId, @RequestBody
-    CreateTravelScheduleRequest createTravelScheduleRequest) {
+    public ApiResult<Boolean> createTravelSchedule(@PathVariable long travelId,
+                                                   @RequestPart("data") CreateTravelScheduleRequest createTravelScheduleRequest,
+                                                   @RequestPart(value = "image", required = false) MultipartFile imageFile)
+            throws IOException {
 
-        CreateTravelScheduleCommand command = createTravelScheduleRequest.toCommand(travelId);
+        CreateTravelScheduleCommand command = createTravelScheduleRequest.toCommand(travelId, imageFile);
         createTravelScheduleUseCase.createTravelSchedule(command);
         return success(true);
     }
--- backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/in/web/request/CreateTravelScheduleRequest.java
+++ backend/src/main/java/com/ssafy/moyeobang/schedule/adapter/in/web/request/CreateTravelScheduleRequest.java
@@ -2,21 +2,22 @@ package com.ssafy.moyeobang.schedule.adapter.in.web.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ssafy.moyeobang.schedule.application.port.in.CreateTravelScheduleCommand;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import org.springframework.web.multipart.MultipartFile;
 
 
 public record CreateTravelScheduleRequest(String scheduleTitle, LocationInfo scheduleLocation,
                                           @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime scheduleTime,
-                                          String memo, String image_url) {
-
-    public CreateTravelScheduleCommand toCommand(long travelId) {
+                                          String memo) {
+    public CreateTravelScheduleCommand toCommand(long travelId, MultipartFile imageFile) throws IOException {
         return new CreateTravelScheduleCommand(
                 travelId,
                 scheduleTitle,
                 scheduleLocation.toCommand(),
                 scheduleTime,
                 memo,
-                image_url
+                imageFile
         );
     }
 }
