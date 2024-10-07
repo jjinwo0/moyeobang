bb81255d1ff788e67ca0e9866861ba955a0e9d0a
Jinwoo <zvzv9808@gmail.com>
2024-10-08 00:05:51 +0900

    [REFACTOR] #168 Token 관련 설정 수정

--- backend/src/main/java/com/ssafy/moyeobang/common/config/jwt/JwtProperties.java
+++ backend/src/main/java/com/ssafy/moyeobang/common/config/jwt/JwtProperties.java
@@ -1,10 +1,11 @@
 package com.ssafy.moyeobang.common.config.jwt;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@Getter
+@Getter @Setter
 @Component
 @ConfigurationProperties("spring.jwt")
 public class JwtProperties {
