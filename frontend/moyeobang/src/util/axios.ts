2319cfd69777c8906b462df8eac0a1a8cd858e51
dhkang0912 <dhkang960912@gmail.com>
2024-10-09 02:38:49 +0900

    [FEAT] #155 기본 axios 설정

--- frontend/moyeobang/src/util/axios.ts
+++ frontend/moyeobang/src/util/axios.ts
@@ -1,11 +1,147 @@
+// 쿠키로 시도
 import axios from 'axios';
+import {getCookie, removeCookie, setCookie} from './cookie';
+// axios 인스턴스를 반환하는 함수
 
-// 배포시 BASE URL 확인할 것, .env 파일에 VITE_BASEURL = {실제 서버 BASE URL} 추가
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASEURL + '/api',
-  responseType: 'json',
+  headers: {'Content-Type': 'application/json'},
   timeout: 4000,
-//   withCredentials: true,  //로그인 구현 후 사용 쿠키와 같은 자격 증명을 요청
 });
 
+// 요청 인터셉터 설정
+instance.interceptors.request.use(
+  async config => {
+    const accessToken = getCookie('accessToken');
+    console.log(' 여긴 오나 ? accessToken', accessToken);
+    if (accessToken) {
+      console.log('accessToken', accessToken);
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터 설정
+instance.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+    const refreshToken = getCookie('refresh-token');
+    console.log('refreshToken', refreshToken);
+
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      if (!refreshToken) {
+        console.error('No refresh token available');
+
+        return Promise.reject(error);
+      }
+
+      try {
+        const refreshToken = getCookie('refresh-token');
+        const response = await instance.get(`/reissue/token`, {
+          headers: {Authorization: `Bearer ${refreshToken}`},
+        });
+
+        if (response) {
+          console.log('response', response);
+          const newAccessToken = response.data.accessToken;
+          console.log('newAccessToken', newAccessToken);
+          setCookie('accessToken', newAccessToken, 900);
+
+          originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+          return instance(originalRequest);
+        }
+      } catch (refreshError) {
+        console.error('Failed to refresh access token:', refreshError);
+        removeCookie('accessToken');
+        removeCookie('refreshToken');
+        window.location.href = '/entrance';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
+
+// 다시 sessionStorage로 시도, 성공 버전
+// import axios from 'axios';
+// import {getCookie, removeCookie, setCookie} from './cookie';
+// // axios 인스턴스를 반환하는 함수
+
+// const instance = axios.create({
+//   baseURL: import.meta.env.VITE_BASEURL + '/api',
+//   headers: {'Content-Type': 'application/json'},
+//   timeout: 4000,
+// });
+
+// // 요청 인터셉터 설정
+// instance.interceptors.request.use(
+//   async config => {
+//     const accessToken = localStorage.getItem('accessToken');
+
+//     if (accessToken) {
+//       config.headers['Authorization'] = `Bearer ${accessToken}`;
+//     }
+//     return config;
+//   },
+//   error => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// // 응답 인터셉터 설정
+// instance.interceptors.response.use(
+//   response => response,
+//   async error => {
+//     const originalRequest = error.config;
+//     const refreshToken = getCookie('refreshToken');
+//     if (
+//       error.response &&
+//       error.response.status === 401 &&
+//       !originalRequest._retry
+//     ) {
+//       originalRequest._retry = true;
+
+//       if (!refreshToken) {
+//         console.error('No refresh token available');
+
+//         return Promise.reject(error);
+//       }
+
+//       try {
+//         const refreshToken = localStorage.getItem('refreshToken');
+//         const response = await instance.get(`/reissue/token`, {
+//           headers: {Authorization: refreshToken},
+//         });
+
+//         if (response) {
+//           console.log('response', response);
+//           const newAccessToken = response.data.accessToken;
+//           localStorage.setItem('accessToken', newAccessToken);
+
+//           originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+//           return instance(originalRequest);
+//         }
+//       } catch (refreshError) {
+//         console.error('Failed to refresh access token:', refreshError);
+//         localStorage.removeItem('accessToken');
+//         localStorage.removeItem('refreshToken');
+//         window.location.href = '/entrance';
+//       }
+//     }
+//     return Promise.reject(error);
+//   }
+// );
+
+// export default instance;
