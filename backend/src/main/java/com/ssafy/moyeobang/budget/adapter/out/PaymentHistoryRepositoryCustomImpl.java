aeb3f6fab37a1e3d32484dcb211dd790d36ef725
김두열 <enduf768640@gmail.com>
2024-10-08 15:24:43 +0900

    [FIX] #172 일정 별 예산 예측 어댑터 수정

--- backend/src/main/java/com/ssafy/moyeobang/budget/adapter/out/PaymentHistoryRepositoryCustomImpl.java
+++ backend/src/main/java/com/ssafy/moyeobang/budget/adapter/out/PaymentHistoryRepositoryCustomImpl.java
@@ -1,16 +1,13 @@
 package com.ssafy.moyeobang.budget.adapter.out;
 
-import static org.elasticsearch.index.query.QueryBuilders.boolQuery;
-import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import static org.springframework.data.elasticsearch.core.query.Criteria.where;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
-import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.Criteria;
+import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -21,44 +18,34 @@ public class PaymentHistoryRepositoryCustomImpl implements PaymentHistoryReposit
 
     @Override
     public List<PaymentHistory> findBy(String placeName, List<Member> members) {
-        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
-                .withQuery(combinedQuery(placeName, members))
-                .build();
-
-        return elasticsearchOperations.search(searchQuery, PaymentHistory.class).stream()
+        return elasticsearchOperations.search(combinedQuery(placeName, members), PaymentHistory.class).stream()
                 .map(SearchHit::getContent)
                 .toList();
     }
 
-    private BoolQueryBuilder combinedQuery(String placeName, List<Member> members) {
-        return boolQuery()
-                .must(placeNameQuery(placeName))
-                .must(membersCountQuery(members))
-                .should(membersQuery(members));
+    private CriteriaQuery combinedQuery(String placeName, List<Member> members) {
+        return new CriteriaQuery(wherePlaceNameIs(placeName)
+                .and(whereMemberCountIs(members.size()))
+                .and(whereMembersIs(members)));
     }
 
-    private MatchQueryBuilder placeNameQuery(String placeName) {
-        return matchQuery("placeName", placeName)
-                .fuzziness("AUTO");
+    private Criteria wherePlaceNameIs(String placeName) {
+        return where("placeName").fuzzy(placeName);
     }
 
-    private MatchQueryBuilder membersCountQuery(List<Member> members) {
-        return matchQuery("member.size", members.size());
+    private Criteria whereMemberCountIs(int memberCount) {
+        return where("member.size").is(memberCount);
     }
 
-    private BoolQueryBuilder membersQuery(List<Member> members) {
-        BoolQueryBuilder membersQuery = boolQuery();
-
-        members.stream()
-                .map(this::memberQuery)
-                .forEach(membersQuery::should);
-
-        return membersQuery;
+    private Criteria whereMembersIs(List<Member> members) {
+        return members.stream()
+                .map(this::whereMemberIs)
+                .reduce(Criteria::or)
+                .orElseThrow();
     }
 
-    private BoolQueryBuilder memberQuery(Member member) {
-        return boolQuery()
-                .must(matchQuery("member.gender", member.getGender()))
-                .must(matchQuery("member.age", member.getAge()));
+    private Criteria whereMemberIs(Member member) {
+        return where("member.gender").is(member.getGender())
+                .and("member.age").is(member.getAge());
     }
 }
