f8037b08606d99d3de31c9cdd76ef5fbbc7204de
jiyeoniing <apple20011215@gmail.com>
2024-10-10 04:44:31 +0900

    [FEAT] tanstackdevtools 주석

--- frontend/moyeobang/src/routes/__root.tsx
+++ frontend/moyeobang/src/routes/__root.tsx
@@ -13,7 +13,7 @@ export const Route = createRootRoute({
     return (
         <ConnectAccountProvider>
           <Outlet />
-          <TanStackRouterDevtools />
+          {/* <TanStackRouterDevtools /> */}
         </ConnectAccountProvider>
     );
   },
