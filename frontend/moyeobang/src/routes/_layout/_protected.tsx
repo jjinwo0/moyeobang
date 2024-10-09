63aeace5fab8fe88475b36a3d722e13bb573a6e6
dhkang0912 <dhkang960912@gmail.com>
2024-10-09 20:41:48 +0900

    [FEAT] #master 홈페이지 보호

--- frontend/moyeobang/src/routes/_layout/_protected.tsx
+++ frontend/moyeobang/src/routes/_layout/_protected.tsx
@@ -1,11 +1,26 @@
-import { createFileRoute, Outlet } from '@tanstack/react-router'
-import React from 'react';
+import { createFileRoute, Outlet, useNavigate } from '@tanstack/react-router'
+import { getCookie } from '@/uti, useNavigatel/cookie'; 
+import React, { useEffect } from 'react';
 
-export const Route = createFileRoute('/_layout/_protected')({
-  component: Protected
-})
 
 // 로그인 된 사용자 아닐시 entrance로 이동
+// 로그인 여부를 확인하는 함수
+const isAuthenticated = () => {
+  return Boolean(getCookie('refresh-token'));
+};
+
+export const Route = createFileRoute('/_layout/_protected')({
+  component: Protected,
+});
+
 function Protected() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isAuthenticated()) {
+      navigate({ to: '/', replace: true });
+    }
+  }, [navigate]);
+
   return <Outlet />;
 }
