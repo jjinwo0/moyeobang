482f9d05623407f8a4505c851e196371b7d3e824
jinwoo <zvzv9808@gmail.com>
2024-10-10 15:35:45 +0900

    [FEAT] #193 여행 종료 다음 날 자동 정산 - Spring Batch

--- backend/src/main/java/com/ssafy/moyeobang/account/adapter/out/persistence/account/TravelAccountRepositoryInAccount.java
+++ backend/src/main/java/com/ssafy/moyeobang/account/adapter/out/persistence/account/TravelAccountRepositoryInAccount.java
@@ -1,10 +1,16 @@
 package com.ssafy.moyeobang.account.adapter.out.persistence.account;
 
 import com.ssafy.moyeobang.common.persistenceentity.travel.TravelAccountJpaEntity;
+import com.ssafy.moyeobang.common.persistenceentity.travel.TravelJpaEntity;
+import io.lettuce.core.dynamic.annotation.Param;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TravelAccountRepositoryInAccount extends JpaRepository<TravelAccountJpaEntity, Long> {
 
     Optional<TravelAccountJpaEntity> findByAccountNumber(String accountNumber);
+
+    @Query("select ta.travel from TravelAccountJpaEntity ta join fetch ta.travel where ta.id = :id")
+    Optional<TravelJpaEntity> findTravelById(@Param("id") Long id);
 }
