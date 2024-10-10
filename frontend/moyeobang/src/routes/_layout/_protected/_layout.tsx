94c908ffd99a23bd61cd3a88773cf1bab454e0ed
유지연 <apple20011215@gmail.com>
2024-10-10 10:34:32 +0900

    [FEAT] 환불 api 주석

--- frontend/moyeobang/src/routes/_layout/_protected/_layout.tsx
+++ frontend/moyeobang/src/routes/_layout/_protected/_layout.tsx
@@ -7,11 +7,11 @@ import PayModal from '@/components/Account/QR/PayModal';
 import NotificationModal from '@/components/notification/NotificationModal';
 import useCurrentTravelStore from '@/store/useCurrentTravelStore';
 import NotTravelModal from '@/components/Account/QR/NotTravelModal';
-import { useEffect } from 'react';
-import moyeobang from '@/services/moyeobang';
-import { useMutation } from '@tanstack/react-query';
-import { useQueryClient } from '@tanstack/react-query';
-import { useSuspenseQuery } from '@tanstack/react-query';
+// import { useEffect } from 'react';
+// import moyeobang from '@/services/moyeobang';
+// import { useMutation } from '@tanstack/react-query';
+// import { useQueryClient } from '@tanstack/react-query';
+// import { useSuspenseQuery } from '@tanstack/react-query';
 import useMyInfo from '@/store/useMyInfoStore';
 import { getCookie } from '@/util/cookie';
 
@@ -30,38 +30,38 @@ export default function Header() {
   const [isAlarmOpen, setIsAlarmOpen] = useState(false);
   const {accountId} = useCurrentTravelStore();
 
-  const queryClient = useQueryClient();
-  const {memberId} = useMyInfo();
-  const token = getCookie('accessToken');
+  // const queryClient = useQueryClient();
+  // const {memberId} = useMyInfo();
+  // const token = getCookie('accessToken');
 
   const {pathname} = useLocation();
 
 
   // 여행 종료 후 환불 post API
-  const {mutate: postRefund } = useMutation({
-    mutationFn: ({accountId} : {accountId:AccountId}) => 
-      moyeobang.postRefundAccount(accountId),
-    onSuccess: async () => {
-      await queryClient.invalidateQueries({
-        queryKey: ['transactionList', accountId], // detail에 바로 업데이트
-        refetchType: 'all',
-      });
-    },
-  });
+  // const {mutate: postRefund } = useMutation({
+  //   mutationFn: ({accountId} : {accountId:AccountId}) => 
+  //     moyeobang.postRefundAccount(accountId),
+  //   onSuccess: async () => {
+  //     await queryClient.invalidateQueries({
+  //       queryKey: ['transactionList', accountId], // detail에 바로 업데이트
+  //       refetchType: 'all',
+  //     });
+  //   },
+  // });
 
   // 여행 데이터 
-  const {data: travelData} = useSuspenseQuery({
-    queryKey: ['travelList', memberId],
-    queryFn: () => {
-      return moyeobang.getTravelList(memberId); 
-    },
-  });
-  const travelList = travelData.data.data; // "endDate": "2023-09-05T12:34:56Z"
+  // const {data: travelData} = useSuspenseQuery({
+  //   queryKey: ['travelList', memberId],
+  //   queryFn: () => {
+  //     return moyeobang.getTravelList(memberId); 
+  //   },
+  // });
+  // const travelList = travelData.data.data; // "endDate": "2023-09-05T12:34:56Z"
 
-  // 날짜만 반환 함수
-  const normalizeDate = (date: Date) => {
-    return new Date(date.getFullYear(), date.getMonth(), date.getDate());
-  };
+  // // 날짜만 반환 함수
+  // const normalizeDate = (date: Date) => {
+  //   return new Date(date.getFullYear(), date.getMonth(), date.getDate());
+  // };
 
   const hideHeader =
     pathname.includes('/detail') ||
@@ -76,28 +76,21 @@ export default function Header() {
     setIsQROpen(prev => !prev);
   }
 
- useEffect(()=>{
-
-  if (!memberId || !accountId || !travelList) return;
-
-  // 현재 시간
-  const nowDate = new Date();
-
-  travelList.map((travel) => {
-
-    //  여행 끝 날짜
-    const travelEndDate = normalizeDate(new Date(travel.endDate));
-
-    // 일자 차이 계산 
-    const dayDiff = (nowDate.getTime()-travelEndDate.getTime()) / (1000 * 60 * 60 * 24);
-
-    // 이미 지난 여행이면
-    if ( dayDiff>0 && dayDiff<=7) {
-      postRefund({accountId})
-    }
-  })
-
-  },[accountId, memberId,travelList,token])
+//  useEffect(()=>{
+//   if (!memberId || !accountId || !travelList) return;
+//   // 현재 시간
+//   const nowDate = new Date();
+//   travelList.map((travel) => {
+//     //  여행 끝 날짜
+//     const travelEndDate = normalizeDate(new Date(travel.endDate));
+//     // 일자 차이 계산 
+//     const dayDiff = (nowDate.getTime()-travelEndDate.getTime()) / (1000 * 60 * 60 * 24);
+//     // 이미 지난 여행이면
+//     if ( dayDiff>0 && dayDiff<=7) {
+//       postRefund({accountId})
+//     }
+//   })
+//   },[accountId, memberId,travelList,token])
 
   return (
     <>
